21c4c9358de5935638cc5954c41c95608c745032
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2022-12-18 13:37:25 +0900

    footer.phpの作成

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="l-footer c-background--footer">
+            <ul class="l-footer--menu c-title--footer c-font-family--mplus1">
+                <li class="c-title--vertical-line">ショップ情報</li>
+                <li>ヒストリー</li>
+            </ul>
+            <small class="c-text--copy-right c-font-family--mplus1 c-font-weight--normal">Copyright: RaiseTech</small>
+        </footer>
+
+    </article>
+</body>
+
+</html>
