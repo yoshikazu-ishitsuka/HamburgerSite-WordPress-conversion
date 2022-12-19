51ff9313da0fe38672896c9bb2c87cdc4b05abf5
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2022-12-19 20:49:40 +0900

    searchform.phpの作成

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<div class="p-search--form">
+    <form action="<?php echo home_url('/'); ?>" method="get" class="c-form-search">
+        <label for="#" class="screen-reader-text">検索</label>
+        <input type="search" class="c-form-search__keyword c-img--magnifying-glass" placeholder=" " name="s" id="s">
+        <input type="submit" value="検索"
+            class="c-form-search__submit c-font-family--mplus1 c-font-color--base c-font-weight--bold">
+    </form>
+</div>
