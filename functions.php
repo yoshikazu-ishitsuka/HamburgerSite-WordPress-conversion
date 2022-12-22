757a460b014adb90cf21bfa372af2f11cefc20bb
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2022-12-23 00:08:01 +0900

    functions.phpの編集

--- functions.php
+++ functions.php
@@ -2,6 +2,17 @@
     // テーマサポート
     add_theme_support('title-tag');
 
+    // タイトル出力
+    function hamburgersite_title($title){
+        if(is_front_page() && is_home()){
+            $title = get_bloginfo('name', 'display');
+        } elseif (is_singular()){
+            $title = single_post_title('', false);
+        }
+            return $title;
+    }
+    add_filter('pre_get_document_title', 'hamburgersite_title');
+
     function hamburgersite_script(){
         wp_enqueue_style('M+PLUS+1p','https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&display=swap');
         wp_enqueue_style('Roboto', 'https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap');
