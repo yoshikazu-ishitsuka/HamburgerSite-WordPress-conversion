ff64dd75e1c042ae9b56f72d05278b1b215f3949
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2022-12-26 22:14:34 +0900

    footer.phpの編集

--- footer.php
+++ footer.php
@@ -1,12 +1,13 @@
 <footer class="l-footer c-background--footer">
-            <ul class="l-footer--menu c-title--footer c-font-family--mplus1">
-                <li class="c-title--vertical-line">ショップ情報</li>
-                <li>ヒストリー</li>
-            </ul>
-            <small class="c-text--copy-right c-font-family--mplus1 c-font-weight--normal">Copyright: RaiseTech</small>
-        </footer>
+    <ul class="l-footer--menu c-title--footer c-font-family--mplus1">
+        <li class="c-title--vertical-line">ショップ情報</li>
+        <li>ヒストリー</li>
+    </ul>
+    <small class="c-text--copy-right c-font-family--mplus1 c-font-weight--normal">Copyright: RaiseTech</small>
+</footer>
 
-    </article>
-    <?php wp_footer();?>
+</article>
+<?php wp_footer(); ?>
 </body>
+
 </html>
