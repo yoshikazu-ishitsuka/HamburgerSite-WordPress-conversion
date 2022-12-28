e05a4d72fae13f0a23e1b8b51a4b5b96d277f58f
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2022-12-28 21:22:55 +0900

    index.phpの編集

--- index.php
+++ index.php
@@ -1,80 +1,70 @@
 <?php get_header(); ?>
-    <?php get_sidebar(); ?>
-        <main class="l-main">
-            <article class="p-visual">
-                <div class="p-visual--main">
-                    <h2 class="c-title--main c-font-color--white c-font-family--mplus1 c-font-weight--bold">
-                        ダミーサイト
+<?php get_sidebar(); ?>
+<main class="l-main">
+    <article class="p-visual">
+        <div class="p-visual--main">
+            <h2 class="c-title--main c-font-color--white c-font-family--mplus1 c-font-weight--bold">
+                ダミーサイト
+            </h2>
+        </div>
+        <ul class="p-visual--sub">
+            <li>
+                <div class="p-visual--sub--takeOut">
+                    <h2 class="c-title--sub c-font-color--ea c-font-family--robot c-font-weight--bold c-title--underline">
+                        Take Out
                     </h2>
+                    <div class="sub-section">
+                        <dl class="p-visual--sub--sentence c-background-color--white">
+                            <dt class="c-title--sentence c-font-color--3c c-font-family--mplus1 c-font-weight--bold">
+                                Take OUT</dt>
+                            <dd class="c-text--sub c-font-weight--normal c-font-color--3c c-font-family--mplus1">
+                                当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています
+                            </dd>
+                        </dl>
+                        <dl class="p-visual--sub--sentence c-background-color--white">
+                            <dt class="c-title--sentence c-font-color--3c c-font-family--mplus1 c-font-weight--bold">
+                                Take OUT</dt>
+                            <dd class="c-text--sub c-font-weight--normal c-font-color--3c c-font-family--mplus1">
+                                テイクアウトで利用できる商品を掲載し当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています
+                            </dd>
+                        </dl>
+                    </div>
                 </div>
-                <ul class="p-visual--sub">
-                    <li>
-                        <div class="p-visual--sub--takeOut">
-                            <h2
-                                class="c-title--sub c-font-color--ea c-font-family--robot c-font-weight--bold c-title--underline">
-                                Take Out
-                            </h2>
-                            <div class="sub-section">
-                                <dl class="p-visual--sub--sentence c-background-color--white">
-                                    <dt
-                                        class="c-title--sentence c-font-color--3c c-font-family--mplus1 c-font-weight--bold">
-                                        Take OUT</dt>
-                                    <dd
-                                        class="c-text--sub c-font-weight--normal c-font-color--3c c-font-family--mplus1">
-                                        当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています
-                                    </dd>
-                                </dl>
-                                <dl class="p-visual--sub--sentence c-background-color--white">
-                                    <dt
-                                        class="c-title--sentence c-font-color--3c c-font-family--mplus1 c-font-weight--bold">
-                                        Take OUT</dt>
-                                    <dd
-                                        class="c-text--sub c-font-weight--normal c-font-color--3c c-font-family--mplus1">
-                                        テイクアウトで利用できる商品を掲載し当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています
-                                    </dd>
-                                </dl>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="p-visual--sub--eatIn">
-                            <h2
-                                class="c-title--sub c-font-color--ea c-font-family--robot c-font-weight--bold c-title--underline">
+            </li>
+            <li>
+                <div class="p-visual--sub--eatIn">
+                    <h2 class="c-title--sub c-font-color--ea c-font-family--robot c-font-weight--bold c-title--underline">
+                        Eat In
+                    </h2>
+                    <div class="sub-section">
+                        <dl class="p-visual--sub--sentence c-background-color--white">
+                            <dt class="c-title--sentence c-font-color--3c c-font-family--mplus1 c-font-weight--bold">
                                 Eat In
-                            </h2>
-                            <div class="sub-section">
-                                <dl class="p-visual--sub--sentence c-background-color--white">
-                                    <dt
-                                        class="c-title--sentence c-font-color--3c c-font-family--mplus1 c-font-weight--bold">
-                                        Eat In
-                                    </dt>
-                                    <dd
-                                        class="c-text--sub c-font-weight--normal c-font-color--3c c-font-family--mplus1">
-                                        店内でお食事いただけるメニューです店内でお食事いただけるメニューです店内でお食事いただけるメニューです店内でお食事いただけるメニューです店内でお食事いただけるメニューです
-                                    </dd>
-                                </dl>
-                                <dl class="p-visual--sub--sentence c-background-color--white">
-                                    <dt
-                                        class="c-title--sentence c-font-color--3c c-font-family--mplus1 c-font-weight--bold">
-                                        Eat In</dt>
-                                    <dd
-                                        class="c-text--sub c-font-weight--normal c-font-color--3c c-font-family--mplus1">
-                                        店内でお食事いただけるメニューです店内でお食事いただけるメニューです</dd>
-                                </dl>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </article>
-
-            <section class="p-access">
-                <div class="p-access--map c-background--map c-background--overlay">
-                    <h2 class="c-title--access c-font-family--mplus1 c-font-weight--bold c-title--underline">見出しが入ります
-                    </h2>
-                    <p class="c-text--access c-font-weight--bold c-font-family--mplus1">
-                        テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                    </p>
+                            </dt>
+                            <dd class="c-text--sub c-font-weight--normal c-font-color--3c c-font-family--mplus1">
+                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです店内でお食事いただけるメニューです店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                            </dd>
+                        </dl>
+                        <dl class="p-visual--sub--sentence c-background-color--white">
+                            <dt class="c-title--sentence c-font-color--3c c-font-family--mplus1 c-font-weight--bold">
+                                Eat In</dt>
+                            <dd class="c-text--sub c-font-weight--normal c-font-color--3c c-font-family--mplus1">
+                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです</dd>
+                        </dl>
+                    </div>
                 </div>
-            </section>
-        </main>
+            </li>
+        </ul>
+    </article>
+
+    <section class="p-access">
+        <div class="p-access--map c-background--map c-background--overlay">
+            <h2 class="c-title--access c-font-family--mplus1 c-font-weight--bold c-title--underline">見出しが入ります
+            </h2>
+            <p class="c-text--access c-font-weight--bold c-font-family--mplus1">
+                テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+            </p>
+        </div>
+    </section>
+</main>
 <?php get_footer(); ?>
