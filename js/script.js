888d32022f243594a451ab9eb6a8ad21ba515f40
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-01-05 21:27:36 +0900

    script.jsの編集(WordPressで機能するようにした)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // headerのMenuボタン押下時の処理
-$(function () {
+jQuery(function ($) {
   $(".c-button--menu.js-button").click(function () {
     $(".l-sidebar__main").scrollTop(0);
     $(".wrapper.u-background--overlay").toggleClass("js-height");
@@ -10,13 +10,13 @@ $(function () {
     });
     $(".l-sidebar__main").toggleClass("is-open");
     $("body").css({ position: "fixed", overflow: "scroll" });
-    $(".wrapper").css({ position: "sticky", top: "0" });
+    $(".wrapper").css({ position: "static", top: "0" });
     $(".menu-close.js-button").css({ position: "sticky", top: "0" });
   });
 });
 
 // sidebarのｘボタン押下時の処理
-$(function () {
+jQuery(function ($) {
   $(".menu-close.js-button").click(function () {
     $(".u-background--overlay").toggleClass("js-height");
     $(".side-wrapper").css({
@@ -33,19 +33,21 @@ $(function () {
 });
 
 //サイドバー表示中にブラウザをレスポンシブした時の処理（ｘボタン押下時と同じスタイルを適用）
-$(window).resize(function () {
-  $(".l-sidebar__main").css({
-    "transition-duration": "0s",
-  });
-
-  if (window.matchMedia("(min-width: 1025px)").matches) {
-    $(".l-sidebar__main").removeClass("is-open");
-    $(".u-background--overlay").removeClass("js-height");
-    $(".wrapper").css({ position: "static" });
-    $("body").css({ position: "static", overflow: "inherit" });
-    $(".side-wrapper").css({
-      "z-index": "-1",
-      left: "100%",
+jQuery(function ($) {
+  $(window).resize(function () {
+    $(".l-sidebar__main").css({
+      "transition-duration": "0s",
     });
-  }
+
+    if (window.matchMedia("(min-width: 1025px)").matches) {
+      $(".l-sidebar__main").removeClass("is-open");
+      $(".u-background--overlay").removeClass("js-height");
+      $(".wrapper").css({ position: "static" });
+      $("body").css({ position: "static", overflow: "inherit" });
+      $(".side-wrapper").css({
+        "z-index": "-1",
+        left: "100%",
+      });
+    }
+  });
 });
