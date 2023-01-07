4376660fb75a26b297ec3b031df90fbda4dcb41d
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-01-07 22:52:38 +0900

    header.phpの編集

--- header.php
+++ header.php
@@ -6,20 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>img/favicon.ico">
-    <!-- <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="./js/jquery-3.6.1.min.js"></script>
-    <script src="./js/script.js"></script> -->
-    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> -->
-    <!-- <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&display=swap" rel="stylesheet"> -->
-    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"> -->
-    <!-- <title>Hamburger Site</title> -->
     <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
     <article class="wrapper u-background--overlay js-toggle">
 
         <header class="l-header c-background-color--base">
