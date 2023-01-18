93835b765b9c8d89ca57e44b7b2d3c35a2f1c440
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-01-18 21:51:33 +0900

    sidebar.phpと各cssの編集

--- sidebar.php
+++ sidebar.php
@@ -2,33 +2,19 @@
     <aside class="l-sidebar p-sideMenu">
         <div class="l-sidebar__main p-sideMenu__main js-toggle">
             <span class="menu-close js-button is-click"><i class="c-img--xmark"></i></span>
-            <h2 class="c-title--sidebar">Menu</h2>
-            <h3 class="c-title--sidebar__menu">バーガー</h3>
-            <ul class="c-text--sidebar">
-                <li>ハンバーガー</li>
-                <li>チーズバーガー</li>
-                <li>テリヤキバーガー</li>
-                <li>アボカドバーガー</li>
-                <li>フィッシュバーガー</li>
-                <li>ベーコンバーガー</li>
-                <li>チキンバーガー</li>
-            </ul>
-            <h3 class="c-title--sidebar__menu">サイド</h3>
-            <ul class="c-text--sidebar">
-                <li>ポテト</li>
-                <li>サラダ</li>
-                <li>ナゲット</li>
-                <li>コーン</li>
-            </ul>
-            <h3 class="c-title--sidebar__menu">ドリンク</h3>
-            <ul class="c-text--sidebar">
-                <li>コーラ</li>
-                <li>ファンタ</li>
-                <li>オレンジ</li>
-                <li>アップル</li>
-                <li>紅茶（Ice/Hot）</li>
-                <li>コーヒー（Ice/Hot）</li>
-            </ul>
+            <?php
+            $args = array(
+                // wp_nav_menu(array(
+                'theme_location'  => 'category_nav',
+                'container'       => 'nav',
+                'container_class' => 'add-class',
+                // 'link_before'     => '',
+                // 'link_after'      => '<i class="icon ion-md-arrow-dropright"></i>',
+                // 'items_wrap'      => '<ul>%3$s</ul>',
+                // ));
+            );
+            wp_nav_menu($args);
+            ?>
         </div>
     </aside>
 </article>
