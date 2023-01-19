86e9e74006a8371b85453197e35ba484995ec789
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-01-19 20:28:21 +0900

    sidebar.phpの編集

--- sidebar.php
+++ sidebar.php
@@ -3,17 +3,15 @@
         <div class="l-sidebar__main p-sideMenu__main js-toggle">
             <span class="menu-close js-button is-click"><i class="c-img--xmark"></i></span>
             <?php
-            $args = array(
-                // wp_nav_menu(array(
+            wp_nav_menu(array(
+                'menu'            => 'categorymenu',
                 'theme_location'  => 'category_nav',
                 'container'       => 'nav',
-                'container_class' => 'add-class',
-                // 'link_before'     => '',
-                // 'link_after'      => '<i class="icon ion-md-arrow-dropright"></i>',
-                // 'items_wrap'      => '<ul>%3$s</ul>',
-                // ));
-            );
-            wp_nav_menu($args);
+                'container_class' => '',
+                'menu_class'      => '',
+                'menu_id'         => '',
+                'items_wrap'      => '<ul>%3$s</ul>',
+            ));
             ?>
         </div>
     </aside>
