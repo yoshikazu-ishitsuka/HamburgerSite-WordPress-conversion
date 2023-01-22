138c4d5afb145ba99a7b96a7c39e93a4e9e72f06
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-01-22 12:20:59 +0900

    archive_search.phpの編集

--- archive_search.php
+++ archive_search.php
@@ -1,41 +1,6 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
 
-<article class="side-wrapper">
-  <aside class="l-sidebar p-sideMenu">
-    <div class="l-sidebar__main p-sideMenu__main js-toggle">
-      <span class="menu-close js-button is-click"><i class="c-img--xmark"></i></span>
-      <h2 class="c-title--sidebar">Menu</h2>
-      <h3 class="c-title--sidebar__menu">バーガー</h3>
-      <ul class="c-text--sidebar">
-        <li>ハンバーガー</li>
-        <li>チーズバーガー</li>
-        <li>テリヤキバーガー</li>
-        <li>アボカドバーガー</li>
-        <li>フィッシュバーガー</li>
-        <li>ベーコンバーガー</li>
-        <li>チキンバーガー</li>
-      </ul>
-      <h3 class="c-title--sidebar__menu">サイド</h3>
-      <ul class="c-text--sidebar">
-        <li>ポテト</li>
-        <li>サラダ</li>
-        <li>ナゲット</li>
-        <li>コーン</li>
-      </ul>
-      <h3 class="c-title--sidebar__menu">ドリンク</h3>
-      <ul class="c-text--sidebar">
-        <li>コーラ</li>
-        <li>ファンタ</li>
-        <li>オレンジ</li>
-        <li>アップル</li>
-        <li>紅茶（Ice/Hot）</li>
-        <li>コーヒー（Ice/Hot）</li>
-      </ul>
-    </div>
-  </aside>
-</article>
-
 <main class="l-main">
   <article class="p-visual__archive">
     <section class="p-visual__archive--main u-background--overlay--archive">
