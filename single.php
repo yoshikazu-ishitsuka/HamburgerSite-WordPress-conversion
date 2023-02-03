e2d0d99cdcd125eaf160fe859217bb4c8338d26f
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-02-03 21:45:48 +0900

    single.phpの編集

--- single.php
+++ single.php
@@ -6,11 +6,18 @@
     <section>
       <?php echo get_the_post_thumbnail('', 'large', ['class' => 'p-visual__single--main']); ?>
       <h1 class="c-title--main c-font-color--white c-font-family--mplus1 c-font-weight--bold">
-        <?php echo get_the_title(); ?>
+        <?php the_title(); ?>
       </h1>
     </section>
 
-    <?php get_template_part("components/description"); ?>
+    <?php if (have_posts()) : ?>
+      <?php while (have_posts()) : the_post(); ?>
+        <?php the_content(); ?>
+      <?php endwhile; ?>
+    <?php else : ?>
+      <p>表示するメニューがありません</p>
+    <?php endif; ?>
+
 
   </article>
 </main>
