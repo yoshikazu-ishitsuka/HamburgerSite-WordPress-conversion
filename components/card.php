cd7cf7b387d25b542eae2320cf68dc5df5336b61
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-02-05 22:02:56 +0900

    card.phpの編集

--- components/card.php
+++ components/card.php
@@ -2,7 +2,7 @@
     <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
             <figure class="p-card__menu">
-                <?php the_post_thumbnail('large', ['class' => 'p-card__menu--image']); ?>
+                <?php the_post_thumbnail('full', ['class' => 'p-card__menu--image']); ?>
                 <figcaption class="p-card__menu--caption c-background-color--card">
                     <h2 class="c-title--card c-font-family--mplus1"><?php the_title(); ?></h2>
                     <h3 class="c-title--subheading c-font-family--mplus1">小見出しが入ります</h3>
