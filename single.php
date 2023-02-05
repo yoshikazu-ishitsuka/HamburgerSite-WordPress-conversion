2e392f285844b41463598dac69fa645793147884
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-02-05 22:03:59 +0900

    single.phpの編集

--- single.php
+++ single.php
@@ -3,12 +3,6 @@
 
 <main class="l-main">
   <article class="p-visual__single">
-    <section>
-      <?php echo get_the_post_thumbnail('', 'large', ['class' => 'p-visual__single--main']); ?>
-      <h1 class="c-title--main c-font-color--white c-font-family--mplus1 c-font-weight--bold">
-        <?php the_title(); ?>
-      </h1>
-    </section>
 
     <?php if (have_posts()) : ?>
       <?php while (have_posts()) : the_post(); ?>
@@ -17,7 +11,6 @@
     <?php else : ?>
       <p>表示するメニューがありません</p>
     <?php endif; ?>
-
 
   </article>
 </main>
