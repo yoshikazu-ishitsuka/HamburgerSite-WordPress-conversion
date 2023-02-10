6711bf36c3e255e8a4abb3a0dad04654ee4ab051
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-02-10 20:54:07 +0900

    404.phpの編集

--- 404.php
+++ 404.php
@@ -7,6 +7,6 @@
     <p class="error_url">URL ：<span><?php echo get_pagenum_link(); ?></span></p>
     <p>現在表示する記事がありません。</p>
     <p>よろしければ、検索ボックスにお探しのコンテンツに該当するキーワードを入力して下さい。</p>
-    <p><a class="page_error__top" href="<?php echo home_url(); ?>">トップページへ戻る</a></p>
+    <p><a class="page_error__top" href="<?php echo esc_url(home_url()); ?>">トップページへ戻る</a></p>
 </div>
 <?php get_footer(); ?>
