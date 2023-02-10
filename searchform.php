e58d9d0415a09a439ebab80159f29ec81d06f848
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-02-10 20:54:30 +0900

    searchform.phpの編集

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <div class="p-search--form">
-    <form action="<?php echo home_url('/'); ?>" method="get" class="c-form-search">
+    <form action="<?php echo esc_url(home_url('/')); ?>" method="get" class="c-form-search">
         <label for="s" class="screen-reader-text">検索</label>
-        <input type="text" class="c-form-search__keyword c-img--magnifying-glass" placeholder=" " name="s" id="s">
+        <input type="text" class="c-form-search__keyword c-img--magnifying-glass" placeholder="検索キーワードを入力" name="s" id="s">
         <input type="submit" value="検索" class="c-form-search__submit c-font-family--mplus1 c-font-color--base c-font-weight--bold">
     </form>
 </div>
