c103cc650df5f4546fff120a149da2b5193869ce
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-02-12 16:38:34 +0900

    category.phpの編集

---
+++ category.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+
+<main class="l-main">
+    <article class="p-visual__archive">
+        <section class="p-visual__archive--main u-background--overlay--archive">
+            <h2 class="c-title--main c-font-color--white c-font-family--robot c-font-weight--bold">
+                Menu:<span class="c-title--under-text"><?php single_cat_title('', true); ?></span>
+            </h2>
+        </section>
+
+        <section class="p-visual__archive--sentence">
+            <h2 class="c-title--archive">
+                <?php single_cat_title('', true); ?>メニューの一覧です
+            </h2>
+            <p class="c-text--archive c-font-family--mplus1">
+                テキストが入ります。
+                テキストが入ります。
+                テキストが入ります。
+                テキストが入ります。
+                テキストが入ります。
+                テキストが入ります。
+                テキストが入ります。
+                テキストが入ります。
+                テキストが入ります。
+                テキストが入ります。
+                テキストが入ります。
+                テキストが入ります。
+                テキストが入ります。
+            </p>
+        </section>
+
+        <?php get_template_part("components/card"); ?>
+
+        <?php wp_pagenavi(); ?>
+
+    </article>
+</main>
+<?php get_footer(); ?>
