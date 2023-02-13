4dd7c670e516677d5da36b4ae4d1a1df0094c887
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-02-13 22:23:32 +0900

    header.phpの編集

--- header.php
+++ header.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>img/favicon.ico">
+    <link rel="shortcut icon" href="<?php echo esc_url(get_template_directory_uri()); ?>img/favicon.ico">
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
     <article class="wrapper u-background--overlay js-toggle">
 
         <header class="l-header c-background-color--base">
