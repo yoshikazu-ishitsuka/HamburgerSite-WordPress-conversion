1bcfec0db7928f66ecf8e097614ce424793c0d73
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-07-30 18:04:41 +0900

    Update: yarpp-template-hamburger.phpの編集

--- yarpp-template-hamburger.php
+++ yarpp-template-hamburger.php
@@ -4,15 +4,6 @@ YARPP Hamburger
 Author: YOSHIKAZU ISHITSUKA
 Description: ハンバーガーサイト関連記事用のテンプレートです。
 */
-?><h3>★ おすすめ商品</h3>
-<?php if (have_posts()):?>
-<ol>
-	<?php while (have_posts()) : the_post(); ?>
-	<li>
-		<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-	</li>
-	<?php endwhile; ?>
-</ol>
-<?php else: ?>
-<p>記事が見つかりません</p>
-<?php endif; ?>
+?>
+<h3>★ おすすめ商品</h3>
+<?php get_template_part("components/card_related"); ?>
